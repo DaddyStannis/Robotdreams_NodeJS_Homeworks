9ee8318de3d4bcffa102ecfdd857192aee34edd7
DaddyStannis <stanislav.sakun@ukr.net>
2025-06-24 23:23:41 +0300

    Added index.js

---
+++ lesson-2/src/index.js
@@ -0,0 +1,19 @@
+import {createServer} from "node:http";
+import url from 'node:url';
+import path from 'node:path';
+import {router, loadRoutes} from "./lib/router.js";
+import {jsonBody} from "./middlewares/json-body.mw.js";
+import {notFound} from "./middlewares/not-found.mw.js";
+import {compose} from "./utils/compose.util.js";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(__filename);
+
+const ROUTES_DIR = path.resolve(__dirname, 'routes');
+const PORT = 4400;
+
+loadRoutes(ROUTES_DIR);
+
+const server = createServer(compose([jsonBody, router, notFound]));
+
+server.listen(PORT, () => console.log(`http://localhost:${PORT}`));
