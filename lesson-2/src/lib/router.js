f42270fe6001d2ef4822f8245fea0789ee12877d
DaddyStannis <stanislav.sakun@ukr.net>
2025-07-06 23:35:12 +0300

    hw 3

--- lesson-2/src/lib/router.js
+++ lesson-2/src/lib/router.js
@@ -1,80 +1,86 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import {pathToRegex} from "../utils/regex.util.js";
+import fs from "node:fs";
+import path from "node:path";
+import { pathToRegex } from "../utils/regex.util.js";
 
 const ENDPOINTS = [];
 
-const ALLOWED_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
+const ALLOWED_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"];
 
 class Param {
-    constructor(idx, name) {
-        this.idx = idx;
-        this.name = name;
-    }
+  constructor(idx, name) {
+    this.idx = idx;
+    this.name = name;
+  }
 }
 
 class Endpoint {
-    constructor(method, handler, pattern, params=[]) {
-        this.pattern = pattern;
-        this.method = method;
-        this.handler = handler;
-        this.params = params;
-    }
+  constructor(method, handler, pattern, params = []) {
+    this.pattern = pattern;
+    this.method = method;
+    this.handler = handler;
+    this.params = params;
+  }
 }
 
-export async function router(req, res){
-    try {
-        const url = new URL(req.url, `http://${req.headers.host}`);
+export async function router(req, res) {
+  try {
+    const url = new URL(req.url, `http://${req.headers.host}`);
 
-        const possibleEndpoints = ENDPOINTS.filter(e => e.pattern.test(url.pathname));
+    const possibleEndpoints = ENDPOINTS.filter((e) =>
+      e.pattern.test(url.pathname)
+    );
 
-        if (!possibleEndpoints.length) {
-            res.statusCode = 404;
-            return res.end('Not Found');
-        }
-
-        for (const endpoint of possibleEndpoints) {
-            if (req.method === endpoint.method) {
-                const segments = url.pathname.split('/').slice(1);
-                req.params = endpoint.params.reduce((acc, param) => ({...acc, [param.name]: segments[param.idx]}), {});
-                return endpoint.handler(req, res);
-            }
-        }
-        res.statusCode = 405;
-        res.end('Method Not Allowed');
-
-    } catch (err) {
-        console.error(err);
-        res.statusCode = 500;
-        res.end('Internal Server Error');
+    if (!possibleEndpoints.length) {
+      res.statusCode = 404;
+      return res.end("Not Found");
     }
+
+    for (const endpoint of possibleEndpoints) {
+      if (req.method === endpoint.method) {
+        const segments = url.pathname.split("/").slice(1);
+        req.params = endpoint.params.reduce(
+          (acc, param) => ({ ...acc, [param.name]: segments[param.idx] }),
+          {}
+        );
+        return endpoint.handler(req, res);
+      }
+    }
+    res.statusCode = 405;
+    res.end("Method Not Allowed");
+  } catch (err) {
+    console.error(err);
+    res.statusCode = 500;
+    res.end("Internal Server Error");
+  }
 }
 
-export function loadRoutes(dir, filePath = '') {
-    for (const file of fs.readdirSync(path.join(dir, filePath))) {
-        const fullPath = path.join(filePath, file);
-        const stats = fs.statSync(path.join(dir, fullPath));
+export function loadRoutes(dir, filePath = "") {
+  for (const file of fs.readdirSync(path.join(dir, filePath))) {
+    const fullPath = path.join(filePath, file);
+    const stats = fs.statSync(path.join(dir, fullPath));
 
-        if (stats.isDirectory()) {
-            loadRoutes(dir, fullPath);
-        } else if (file === 'route.js') {
-            const params = [];
-            const segments = fullPath.split('/');
-            for (const idx in segments) {
-                const segment = segments[idx];
-                if (segment.startsWith('[') && segment.endsWith(']')) {
-                    params.push(new Param(idx, segment.slice(1, -1)));
-                }
-            }
-
-            import(path.join(dir, fullPath)).then(route => {
-                for (const handler of Object.values(route)) {
-                    if (ALLOWED_METHODS.includes(handler.name)) {
-                        const pattern = pathToRegex(fullPath.replace('route.js', ''));
-                        ENDPOINTS.push(new Endpoint(handler.name, handler, pattern, params));
-                    }
-                }
-            })
+    if (stats.isDirectory()) {
+      loadRoutes(dir, fullPath);
+    } else if (file === "route.js") {
+      const params = [];
+      const segments = fullPath.split("/");
+      for (const idx in segments) {
+        const segment = segments[idx];
+        if (segment.startsWith("[") && segment.endsWith("]")) {
+          params.push(new Param(idx, segment.slice(1, -1)));
         }
+      }
+
+      import(path.join(dir, fullPath)).then((route) => {
+        for (const handler of Object.values(route)) {
+          if (ALLOWED_METHODS.includes(handler.name)) {
+            const pattern = pathToRegex(fullPath.replace("route.js", ""));
+            ENDPOINTS.push(
+              new Endpoint(handler.name, handler, pattern, params)
+            );
+          }
+        }
+      });
     }
+  }
 }
---
+++ lesson-3/kv-server/index.js
@@ -0,0 +1,35 @@
+import Fastify from "fastify";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const fastify = Fastify({logger: true});
+
+const REDIS_URL = process.env.REDIS_URL;
+const PORT = process.env.PORT || 3000;
+
+fastify.get("/get/:key", async function handler(request, reply) {
+    const query = new URLSearchParams({
+        key: request.params.key,
+    }).toString();
+    const response = await fetch(REDIS_URL + "/get?" + query);
+    return response.json();
+});
+
+fastify.post("/set", async function handler(request, reply) {
+    const response = await fetch(REDIS_URL + "/set", {
+        method: "POST",
+        body: JSON.stringify(request.body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    return response.json();
+});
+
+try {
+    await fastify.listen({host: '0.0.0.0', port: PORT});
+} catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+}
